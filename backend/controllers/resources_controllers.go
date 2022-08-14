f7206cbed164eeb455edcce997c884f0746c17c3
NullBy1e <68380685+NullBy1e@users.noreply.github.com>
2022-08-14 15:44:19 +0200

    Added archive file support

--- backend/controllers/resources_controllers.go
+++ backend/controllers/resources_controllers.go
@@ -3,8 +3,10 @@ package controllers
 import (
 	"cryptoguess/configs"
 	"cryptoguess/responses"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
@@ -27,7 +29,33 @@ func ResourceGetArchiveFile() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		//* Returns the archival file
 		//! ( For now only to admins !)
-		//TODO: Do it.
-		c.JSON(http.StatusServiceUnavailable, nil)
+
+		File := struct {
+			Name string `uri:"file"`
+		}{}
+
+		if err := c.BindUri(&File); err != nil {
+			c.JSON(http.StatusBadRequest, responses.ResourcesResponse{Message: "Cannot bind URI", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+
+		session := sessions.Default(c)
+		if session.Get("id") == nil {
+			c.JSON(http.StatusForbidden, responses.ResourcesResponse{Message: "You need to be logged in to access archive"})
+			return
+		}
+		if session.Get("admin ") == false {
+			c.JSON(http.StatusForbidden, responses.ResourcesResponse{Message: "You need to be admin to access the archive"})
+			return
+		}
+
+		//? Do we need a input validator to prevent security bugs ?
+		fileContent, err := ioutil.ReadFile("./archive/" + File.Name)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, responses.ResourcesResponse{Message: "Cannot get the provided file", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
+		c.Header("Content-Disposition", "attachment; filename="+File.Name)
+		c.Data(http.StatusOK, "application/octet-stream", fileContent)
 	}
 }
--- backend/responses/resources_responses.go
+++ backend/responses/resources_responses.go
@@ -4,3 +4,8 @@ type DailyCoinResponse struct {
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
+
+type ResourcesResponse struct {
+	Message string                 `json:"message"`
+	Data    map[string]interface{} `json:"data"`
+}
