ef6471fca02d2dca8ed9af3ebb3fda350a339dd9
NullBy1e <68380685+NullBy1e@users.noreply.github.com>
2022-08-23 17:37:39 +0200

    Fixed Merge conflicts

--- frontend/src/API.js
+++
@@ -1,7 +0,0 @@
-const URL = "http://localhost:8000/resources/coins_today";
-
-export async function getTodaysCoins() {
-	const response = await fetch(URL)
-	var data = await response.json()
-	return data
-}
